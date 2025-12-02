a0c97efaf08ffcc65df4023e0391fc463d4e29ef
isanctuarry <khrnsa143@gmail.com>
2025-12-02 13:52:33 +0700

    cb

--- App/Models/Tamu.php
+++ App/Models/Tamu.php
@@ -11,12 +11,12 @@ class Tamu extends Model {
     // Insert tamu baru
     public function tambahTamu($data) {
         $stmt = $this->db->prepare("
-            INSERT INTO tamu (id, nama, tanggal_kunjungan, email) 
+            INSERT INTO tamu (id_tamu, nama, tanggal_kunjungan, email) 
             VALUES (?, ?, ?, ?)
         ");
 
         return $stmt->execute([
-            $data['id'],
+            $data['id_tamu'],
             $data['nama'],
             $data['tanggal_kunjungan'],
             $data['email'],
