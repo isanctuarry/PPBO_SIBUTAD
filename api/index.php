5ad4059a7d6bcde1561347aff890ca926f685329
Nadya1030 <h1101241030@student.untan.ac.id>
2025-12-02 17:53:48 +0700

    usdfus

--- api/index.php
+++ api/index.php
@@ -1,5 +1,12 @@
 <?php  
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 // Force working directory to project root
 chdir(__DIR__ . '/..'); 
-
 require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\App;
+
+// Jalankan router
+$app = new App();
