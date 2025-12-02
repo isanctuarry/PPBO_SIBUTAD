148463ea29c5814075aaf61888d16197e6c49b34
Nadya1030 <h1101241030@student.untan.ac.id>
2025-12-02 17:14:01 +0700

    kukukuku

--- api/index.php
+++ api/index.php
@@ -5,6 +5,7 @@ ini_set('session.cookie_lifetime', 720);
 ini_set('session.gc_maxlifetime', 720); 
 
 session_start();
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Core\App;
--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 
+ini_set('session.cookie_lifetime', 0); 
+ini_set('session.gc_maxlifetime', 387744378); 
 
+session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
