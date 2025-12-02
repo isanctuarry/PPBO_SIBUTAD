e8d7b95da261241c194601f7e0d17e86c54a38fd
Nadya1030 <h1101241030@student.untan.ac.id>
2025-12-02 17:09:17 +0700

    boleh deh yang ini

--- api/index.php
+++ api/index.php
@@ -1,8 +1,10 @@
 <?php  
-
 // Force working directory to project root
 chdir(__DIR__ . '/..'); 
+ini_set('session.cookie_lifetime', 720); 
+ini_set('session.gc_maxlifetime', 720); 
 
+session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Core\App;
--- public/index.php
+++ public/index.php
@@ -1,9 +1,6 @@
 <?php
 
-ini_set('session.cookie_lifetime', 720); 
-ini_set('session.gc_maxlifetime', 720); 
 
-session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
