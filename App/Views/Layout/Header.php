66f33128e3a7fe04c1671e6b93be0d7aa2f5fc5f
Irenensy <h1101241031@student.untan.ac.id>
2025-12-03 21:49:02 +0700

    hm

--- App/Views/Layout/Header.php
+++ App/Views/Layout/Header.php
@@ -4,8 +4,9 @@ $host = $_SERVER['HTTP_HOST'];
 if (strpos($host, 'localhost') !== false) {
   $base_url = 'http://' . $host;
 } else {
-  $base_url = 'https://' . $host . '/';
+  $base_url = 'https://' . $host;
 }
+
 ?>
 <!doctype html>
 <html lang="id">
