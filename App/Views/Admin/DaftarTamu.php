1092f89f0b2ab4677cc86654af4823037610b021
Irenensy <h1101241031@student.untan.ac.id>
2025-12-04 07:50:49 +0700

    fixx

--- App/Views/Admin/DaftarTamu.php
+++ App/Views/Admin/DaftarTamu.php
@@ -1,5 +1,14 @@
 <?php require __DIR__ . '/../Layout/Header.php'; ?>
 <!-- TABEL -->
+ <div style="margin-bottom: 15px;">
+    <button 
+        onclick="history.back()" 
+        class="btn btn-sm btn-outline-secondary" 
+        title="Kembali ke halaman sebelumnya">
+        &larr; Kembali
+    </button>
+</div>
+
 <div class="card p-3">
     <h5 class="mb-3">Daftar Tamu</h5>
 
