5016b6c9936d8b9efb6c420d3cf92a127d1f6c61
isa's sanctuary <khrnsa143@gmail.com>
2025-12-04 14:11:15 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,11 +4,6 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 // Force working directory to project root
 chdir(__DIR__ . '/..'); 
-ini_set('session.cookie_lifetime', 720); 
-ini_set('session.gc_maxlifetime', 720); 
-
-session_start();
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Core\App;
